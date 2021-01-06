13256b1b75c932293b305c5a0819de6036efdad8
Markki Meyer <markkimeyer@gmail.com>
2021-01-05 22:56:49 -0600

    prompts and confirms

--- script.js
+++ script.js
@@ -4,8 +4,39 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  console.log("hello");
-}
+  // strings of data sets
+ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var numbers = 0123456789;
+ var specialChar = "!#$%&'()*+,-/<=>?@\^_";
+
+ // User's requirements
+  var passwordLength = prompt("Enter a length from 8-128");
+
+  var passwordLowerCase = confirm("Do you want to use lowercase letters?");
+  var passwordUpperCase =confirm("Do you want to use uppercase letters?");
+  var passwordNumbers =confirm("Do you want to use numbers?");
+  var passwordSpecialChar =confirm("Do you want to use special characters?");
+
+
+// if statements 
+    if (passwordLowerCase) {
+      alert("you have chosen to use lower case letters");
+    }
+
+    if (passwordUpperCase) {
+      alert("you have chosen to use upper case letters");
+    }
+
+     if (passwordNumbers) {
+      alert("you have chosen to use numbers");
+    }
+
+    if (passwordSpecialChar) {
+      alert("you have chosen to use special characters");
+    }
+
+  }
 
 // Write password to the #password input
 function writePassword() {
