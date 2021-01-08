9326595d7cfca9f65d74673e1380ff99a4624292
Markki Meyer <markkimeyer@gmail.com>
2021-01-07 23:18:20 -0600

    added validation and updated Readme

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var clearBtn = document.querySelector("#clear");
+var passwordText = document.querySelector("#password");
 var minLength = 8;
 var maxLength = 128;
-
-var password = '';
 
 var userPicksArray = [];
 var passwordArray = [];
 
-/
 
 // User's requirements
 
 function generatePassword() {
-
+  
   // strings of data sets
 
- 
+
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var specialChar = "!#$%&'()*+,-/<=>?^@_";
   var passwordLength = prompt("Enter a length from 8-128");
 
-  // prompt if statements 
-  if (passwordLength < 8 || passwordLength > 128) {
-    // alert("Please enter a number between 8-128");
+  //length prompt and verification
+  while (passwordLength < minLength || passwordLength > maxLength) {
     var passwordLength2 = prompt("ERROR: Re-enter a number from 8-128");
     passwordLength = passwordLength2;
   }
-
 
   var passwordLowerCase = confirm("Do you want to use lowercase letters?");
   var passwordUpperCase = confirm("Do you want to use uppercase letters?");
@@ -38,17 +34,17 @@ function generatePassword() {
   var passwordSpecialChar = confirm("Do you want to use special characters?");
 
 
-  // confirm if statements 
+  // confirm if statements using spread operators
   if (passwordLowerCase === true) {
     userPicksArray.push(...lowerCase);
-    
+
   }
 
   if (passwordUpperCase === true) {
     userPicksArray.push(...upperCase);
   }
 
-   if (passwordNumbers === true) {
+  if (passwordNumbers === true) {
     userPicksArray.push(...numbers);
   }
 
@@ -57,35 +53,30 @@ function generatePassword() {
   }
 
 
-  //THIS WORKS FOR ALL LOWER CASE
-   for (i = 0; i < passwordLength; i++) {
-     passwordArray.push(userPicksArray[Math.floor(Math.random() * userPicksArray.length)]);
-     console.log(passwordArray.join(""));
-     
-  
-    //   var char = Math.floor(Math.random() * lowerCase.length);
-  //   password += lowerCase.charAt(char);
+  //fill password slot with random array values
+  for (i = 0; i < passwordLength; i++) {
+    passwordArray.push(userPicksArray[Math.floor(Math.random() * userPicksArray.length)]);
+    console.log(passwordArray.join(""));
   }
-  // alert(password);
-   }
-  //testing random password 
-  // for (i=0; i < passwordLength; i++) {
 
-
-  // Write password to the #password input
-  function writePassword() {
-    generatePassword();
-    var passwordText = document.querySelector("#password");
-
+}
+// Write password to the #password input and validate
+function writePassword() {
+  generatePassword();
+  if (passwordArray == []) {
+    alert("ERROR: You must select at least one criterion");
+  }
+  else {
     passwordText.value = passwordArray.join("");
+  };
+}
+
+//refresh page
+function clearPassword() {
+  window.parent.location = window.parent.location.href;
+}
 
 
-  }
-
-  function clearPassword() {
-    window.parent.location = window.parent.location.href;
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  clearBtn.addEventListener("click", clearPassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+clearBtn.addEventListener("click", clearPassword);
